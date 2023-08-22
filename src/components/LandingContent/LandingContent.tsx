3b25e07f29338117596fc336314449a6f6d72c9e
Wesley <wesleyadwebster@gmail.com>
2023-08-21 22:06:32 -0500

    Positioning of gallery, font updates and color scheme added for landing page

--- src/components/LandingContent/LandingContent.tsx
+++ src/components/LandingContent/LandingContent.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import classes from './LandingContent.module.scss';
 
-const { landingContainer, galleryContainer } = classes;
+const { landingContainer, galleryContainer, landingFlex, blurb } = classes;
 
 console.log(classes)
 
@@ -11,67 +11,53 @@ const LandingContent = ({ }) => {
         {
             imagePath: '/assets/gallery/gallery-image-1.png',
             altTag: 'Girl meditating',
-            width: 400,
-            height: 216,
+            width: 500,
+            height: 316,
             key: 0
         },
         {
             imagePath: '/assets/gallery/gallery-image-2.png',
             altTag: 'A charcuterie board',
-            width: 454,
+            width: 654,
             height: 325,
             key: 1
         },
         {
             imagePath: '/assets/gallery/gallery-image-3.png',
             altTag: 'Close up of a salad',
-            width: 565,
-            height: 272,
+            width: 665,
+            height: 327,
             key: 2
         },
-    ]
-
-    // ! original starting properties
-    // const gallery = [
-    //     {
-    //         imagePath: '/assets/gallery/gallery-image-1.png',
-    //         altTag: 'Girl meditating',
-    //         width: 565,
-    //         height: 316,
-    //         key: 0
-    //     },
-    //     {
-    //         imagePath: '/assets/gallery/gallery-image-2.png',
-    //         altTag: 'A charcuterie board',
-    //         width: 614,
-    //         height: 425,
-    //         key: 1
-    //     },
-    //     {
-    //         imagePath: '/assets/gallery/gallery-image-3.png',
-    //         altTag: 'Close up of a salad',
-    //         width: 765,
-    //         height: 372,
-    //         key: 2
-    //     },
-    // ]
+    ];
 
     return (
         <div className={landingContainer}>
-            <h1>Get cooking, get moving,<br/> get groov&apos;n</h1>
-            <section className={galleryContainer}>
-                {gallery.map((img) => {
-                    return (
-                        <Image
-                            src={img.imagePath}
-                            alt={img.altTag}
-                            key={img.key}
-                            width={img.width}
-                            height={img.height}
-                        />
-                    );
-                })}
-            </section>
+            <h1>Get <span>cooking</span>. 
+            get <span>moving</span>.<br /> 
+            get <span>groov&apos;n.</span></h1>
+            <div className={landingFlex}>
+                <section className={galleryContainer}>
+                    {gallery.map((img) => {
+                        return (
+                            <Image
+                                src={img.imagePath}
+                                alt={img.altTag}
+                                key={img.key}
+                                width={img.width}
+                                height={img.height}
+                            />
+                        );
+                    })}
+                </section>
+                <section className={blurb}>
+                    <h2>Discover a world of healthy living</h2>
+                    <p>
+                    Join or log in now to start your journey to a nourished body and a vibrant community. Let's cook, stay fit, and make lasting connections together!
+                    </p>
+                </section>
+            </div>
+
         </div>
     );
 };
