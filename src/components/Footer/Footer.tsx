9d46cc6c12dd9d8f0b46e766688bce372fb69077
Wesley <wesleyadwebster@gmail.com>
2023-08-21 23:45:27 -0500

    Fixed detail on footer. Added flair of borders to landing page gallery

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = ({ }) => {
         <footer>
             <div className={information}>
                 <div>
-                    <label>Address: 177A Bleecker Street</label>
+                    <label>Address: 177A Bleecker Street,<br/> New York City, NY. 10012</label>
                     <label>Phone: (212)-555-7143</label>
                     <label>Email: inquiries@reviverecipe.com</label>
                 </div>
