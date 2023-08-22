c1d5dbc52f3a3505ceb54101f0c2cc64e22b0b6a
Wesley <wesleyadwebster@gmail.com>
2023-08-21 21:04:21 -0500

    image slightly placed correctly just need to adjust

--- src/components/LandingContent/LandingContent.tsx
+++ src/components/LandingContent/LandingContent.tsx
@@ -3,35 +3,62 @@ import classes from './LandingContent.module.scss';
 
 const { landingContainer, galleryContainer } = classes;
 
+console.log(classes)
+
 const LandingContent = ({ }) => {
 
     const gallery = [
         {
             imagePath: '/assets/gallery/gallery-image-1.png',
             altTag: 'Girl meditating',
-            width: 600,
-            height: 400,
+            width: 400,
+            height: 216,
             key: 0
         },
         {
             imagePath: '/assets/gallery/gallery-image-2.png',
             altTag: 'A charcuterie board',
-            width: 600,
-            height: 400,
+            width: 454,
+            height: 325,
             key: 1
         },
         {
             imagePath: '/assets/gallery/gallery-image-3.png',
             altTag: 'Close up of a salad',
-            width: 600,
-            height: 400,
+            width: 565,
+            height: 272,
             key: 2
         },
     ]
 
+    // ! original starting properties
+    // const gallery = [
+    //     {
+    //         imagePath: '/assets/gallery/gallery-image-1.png',
+    //         altTag: 'Girl meditating',
+    //         width: 565,
+    //         height: 316,
+    //         key: 0
+    //     },
+    //     {
+    //         imagePath: '/assets/gallery/gallery-image-2.png',
+    //         altTag: 'A charcuterie board',
+    //         width: 614,
+    //         height: 425,
+    //         key: 1
+    //     },
+    //     {
+    //         imagePath: '/assets/gallery/gallery-image-3.png',
+    //         altTag: 'Close up of a salad',
+    //         width: 765,
+    //         height: 372,
+    //         key: 2
+    //     },
+    // ]
+
     return (
         <div className={landingContainer}>
-            <h1>Get cooking, get moving, get groov&apos;n</h1>
+            <h1>Get cooking, get moving,<br/> get groov&apos;n</h1>
             <section className={galleryContainer}>
                 {gallery.map((img) => {
                     return (
@@ -43,7 +70,7 @@ const LandingContent = ({ }) => {
                             height={img.height}
                         />
                     );
-                })};
+                })}
             </section>
         </div>
     );
