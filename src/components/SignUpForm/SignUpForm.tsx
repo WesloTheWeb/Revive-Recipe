10bb7ce6e9ab9c05df67b2f8ff97f3b6988cc45a
Wesley <wesleyadwebster@gmail.com>
2023-08-23 00:21:37 -0500

    First iteration of the form done for desktop

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import dynamic from "next/dynamic";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import ReactSelect from "react-select";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Image from 'next/image';
 import classes from './SignUpForm.module.scss';
 import Button, { ButtonTypes } from "@/containers/Button/Button";
+
+const ReactSelectNoSSR = dynamic(() => import("react-select"), {
+    ssr: false,
+    loading: () => <p>Loading...</p>
+});
 
 const { signUpInstructions, signUpContainer, signUpFormContainer, formGroup, actionButtonsContainer, relatedFormGroup } = classes;
 
@@ -11,7 +19,7 @@ interface Inputs {
     firstName: string;
     lastName: string;
     gender: string;
-    birthday: any; // might use datepicker calendar for this.
+    birthday: any;
     username: string;
     password: string;
     email: string;
@@ -23,11 +31,14 @@ const genderOptions: readonly { value: string, label: string }[] = [
     { value: 'other', label: 'Other' },
 ];
 
-const SignUpForm = ({ }) => {
 
+const SignUpForm = ({ }) => {
     // RHF Properties
     const { register, handleSubmit, watch, control, formState: { errors } } = useForm<Inputs>();
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+    const eighteenYearsAgo = new Date();
+    eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
 
     return (
         <section className={signUpContainer}>
@@ -65,7 +76,7 @@ const SignUpForm = ({ }) => {
                             name="gender"
                             control={control}
                             render={({ field }) => (
-                                <ReactSelect
+                                <ReactSelectNoSSR
                                     {...field}
                                     options={genderOptions as any}
                                     isClearable
@@ -77,10 +88,28 @@ const SignUpForm = ({ }) => {
                     </div>
                     <section className={relatedFormGroup}>
                         <div className={formGroup}>
+                            <label>*Birthday (atleast 18 years old)</label>
+                            <Controller
+                                name="birthday"
+                                control={control}
+                                render={({ field }) => (
+                                    <DatePicker
+                                        selected={field.value}
+                                        onChange={(date) => field.onChange(date)}
+                                        dateFormat="MM/dd/yyyy"
+                                        maxDate={eighteenYearsAgo}
+                                        placeholderText="mm/dd/yyyy"
+                                    />
+                                )}
+                            />
+                        </div>
+                        <div className={formGroup}>
                             <label>*Email Address</label>
                             <input placeholder="email address"{...register("email", { required: true })} />
                             {errors.lastName && <span>Email cannot be empty.</span>}
                         </div>
+                    </section>
+                    <section className={relatedFormGroup}>
                         <div className={formGroup}>
                             <label>*Username </label>
                             <input {...register("username", { required: true })} />
@@ -89,15 +118,13 @@ const SignUpForm = ({ }) => {
                         <p>
                             Please ensure it is unique, appropriate and abides by our community guidelines.
                         </p>
+                        <div className={formGroup}>
+                            <label>*Create password</label>
+                            <input {...register("password", { required: true })} />
+                            {errors.password && <span>Password needed.</span>}
+                        </div>
                     </section>
-                    <div className={formGroup}>
-                        <label>*Create password</label>
-                        <input {...register("password", { required: true })} />
-                        {errors.password && <span>Password needed.</span>}
-                    </div>
                 </section>
-
-
                 <section className={actionButtonsContainer}>
                     <Link href="/">
                         <Button buttonType={ButtonTypes.RETURN} />
