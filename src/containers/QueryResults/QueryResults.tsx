dc19ccbd7875f2f13de9328228cffc431cc90e6c
Wesley <wesleyadwebster@gmail.com>
2023-08-27 17:25:36 -0500

    pagination start, index and buttons created

--- src/containers/QueryResults/QueryResults.tsx
+++ src/containers/QueryResults/QueryResults.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store/store';
 import { RecipeData } from '@/interfaces/recipeTypes';
@@ -15,15 +16,24 @@ interface QueryResultsProps {
 
 const QueryResults = ({ showModal, setSelectedRecipeIngredients }: QueryResultsProps) => {
 
+    // Redux States
     const query = useSelector((state: RootState) => state.search.query);
     const searchResults = useSelector((state: RootState): RecipeData[] => state.search.results);
     const loading = useSelector((state: RootState) => state.search.loading);
     const error = useSelector((state: RootState) => state.search.error);
 
+    // component state for pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    // index numbers of last, first and current:
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = searchResults.slice(indexOfFirstItem, indexOfLastItem);
+
     return (
         <>
             <section className={queryHeader}>
-                <label>Results for {query} showing 5 out of {searchResults.length}</label>
+                Results for {query} showing {indexOfFirstItem + 1} to {Math.min(indexOfLastItem, searchResults.length)}
             </section>
             <section>
                 {loading && <div>Loading...</div>}
@@ -55,8 +65,13 @@ const QueryResults = ({ showModal, setSelectedRecipeIngredients }: QueryResultsP
                         }}
                     />
                 ))}
-
             </section>
+            <div>
+                <button onClick={() => setCurrentPage(1)}>First</button>
+                <button onClick={() => setCurrentPage(prevPage => Math.max(prevPage - 1, 1))}>Prev</button>
+                <button onClick={() => setCurrentPage(nextPage => Math.min(nextPage + 1, Math.ceil(searchResults.length / itemsPerPage)))}>Next</button>
+                <button onClick={() => setCurrentPage(Math.ceil(searchResults.length / itemsPerPage))}>Last</button>
+            </div>
         </>
     );
 };
